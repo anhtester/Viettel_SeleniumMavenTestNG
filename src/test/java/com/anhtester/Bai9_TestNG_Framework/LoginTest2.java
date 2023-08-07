c1614574c83495795f11aa219017e8d93b452c39
Anh Tester <thaian.it15@gmail.com>
2023-08-07 17:44:17 +0700

    Bài 9 TestNG Framework

---
+++ src/test/java/com/anhtester/Bai9_TestNG_Framework/LoginTest2.java
@@ -0,0 +1,56 @@
+package com.anhtester.Bai9_TestNG_Framework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginTest2 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setDriver(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+    }
+
+    @AfterMethod
+    public void closeDriver(){
+        driver.quit();
+    }
+
+    @Test
+    public void testLoginCRM_Success(){
+        driver.get("https://crm.anhtester.com/admin/authentication");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("admin@example.com");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+    }
+
+    @Test
+    public void testLoginCRM_WithEmailInvalid(){
+        driver.get("https://crm.anhtester.com/admin/authentication");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("admin123@example.com");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+        Assert.assertEquals(driver.getTitle(), "Dashboard"); //Fail vì không login được
+    }
+
+    @Test
+    public void testLoginCRM_WithPasswordInvalid(){
+        driver.get("https://crm.anhtester.com/admin/authentication");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("admin@example.com");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+    }
+
+}
