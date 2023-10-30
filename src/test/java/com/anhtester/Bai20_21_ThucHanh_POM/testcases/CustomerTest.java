d01697793d23f3d73e95d36fb603459b1c74a15d
Anh Tester <thaian.it15@gmail.com>
2023-10-30 17:57:49 +0700

    Bài 20 21 Thực hành Page Object Model

--- src/test/java/com/anhtester/Bai20_21_ThucHanh_POM/testcases/CustomerTest.java
+++ src/test/java/com/anhtester/Bai20_21_ThucHanh_POM/testcases/CustomerTest.java
@@ -3,6 +3,7 @@ package com.anhtester.Bai20_21_ThucHanh_POM.testcases;
 import com.anhtester.Bai20_21_ThucHanh_POM.pages.CustomerPage;
 import com.anhtester.Bai20_21_ThucHanh_POM.pages.DashboardPage;
 import com.anhtester.Bai20_21_ThucHanh_POM.pages.LoginPage;
+import com.anhtester.Bai20_21_ThucHanh_POM.pages.ProfilePage;
 import com.anhtester.common.BaseTest;
 import com.anhtester.constants.ConfigData;
 import org.testng.annotations.Test;
@@ -12,6 +13,7 @@ public class CustomerTest extends BaseTest {
     LoginPage loginPage;
     DashboardPage dashboardPage;
     CustomerPage customerPage;
+    ProfilePage profilePage;
 
     private String CUSTOMER_NAME = "Viettel A1";
 
@@ -26,6 +28,10 @@ public class CustomerTest extends BaseTest {
         customerPage.verifyRedirectCustomerPageSuccess();
         customerPage.clickButtonAddNew();
         customerPage.inputDataCustomer(CUSTOMER_NAME);
+        dashboardPage.clickMenuCustomers();
+
+        profilePage = customerPage.searchCustomer(CUSTOMER_NAME);
+        profilePage.verifyCustomerDetail(CUSTOMER_NAME);
 
     }
 }
