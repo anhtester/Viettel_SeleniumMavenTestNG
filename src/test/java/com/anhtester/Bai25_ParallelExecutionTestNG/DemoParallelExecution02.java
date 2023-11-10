2c971a73e77cce5e89b13b2294cb4438b0ccfeea
Anh Tester <thaian.it15@gmail.com>
2023-11-10 17:33:41 +0700

    Bài 25 - Parallel Execution in TestNG

---
+++ src/test/java/com/anhtester/Bai25_ParallelExecutionTestNG/DemoParallelExecution02.java
@@ -0,0 +1,45 @@
+package com.anhtester.Bai25_ParallelExecutionTestNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DemoParallelExecution02 {
+    @Test
+    public void FirefoxTest() throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://anhtester.com");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//h3[normalize-space()='Website Testing']")).click();
+        driver.quit();
+    }
+
+    @Test
+    public void EdgeTest() throws InterruptedException {
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://anhtester.com");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//h3[normalize-space()='Website Testing']")).click();
+        driver.quit();
+    }
+
+    @Test
+    public void ChromeTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://anhtester.com");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//h3[normalize-space()='API Testing']")).click();
+        driver.quit();
+    }
+}
